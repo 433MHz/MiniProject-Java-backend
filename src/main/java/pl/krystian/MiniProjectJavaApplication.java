13f4e72ee20d8620b761d3513ebbae2b5d5f3614
Krystian <krystian.izdebski1999@gmail.com>
2021-01-02 17:05:17 +0100

    first init

---
+++ src/main/java/pl/krystian/MiniProjectJavaApplication.java
@@ -0,0 +1,13 @@
+package pl.krystian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjectJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/krystian/MiniProjectJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.krystian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
